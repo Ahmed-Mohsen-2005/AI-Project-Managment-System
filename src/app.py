7ecf5da8b23591f4f7f3ed0603908a114863bf16
Ahmed Mohsen <ahmedazab05@gmail.com>
2025-12-01 15:44:57 +0200

    edited app.py

--- src/app.py
+++ src/app.py
@@ -1,24 +1,22 @@
 from flask import Flask
 from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
-from controllers.user_controller import user_bp
+from controllers.sprint_controller import sprint_bp
+from controllers.task_controller import task_bp 
+from controllers.report_controller import report_bp
+from controllers.project_controller import project_bp
+from controllers.notification_controller import notification_bp
+from controllers.integration_controller import integration_bp
+from controllers.file_attachment_controller import file_attachment_bp
+
 app = Flask(__name__)
 app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(user_bp)
+app.register_blueprint(sprint_bp)
+app.register_blueprint(task_bp)
+app.register_blueprint(report_bp)
+app.register_blueprint(project_bp)
+app.register_blueprint(notification_bp)
+app.register_blueprint(integration_bp)
+app.register_blueprint(file_attachment_bp)
 
 @app.route("/")
 def home():
--- src/controllers/file_attachment_controller.py
+++ src/controllers/file_attachment_controller.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, abort, redirect, url_for, jsonify
 from repositories.repository_factory import RepositoryFactory
 
-file_bp = Blueprint("file", __name__, url_prefix="/api/v1/files")
+file_attachment_bp = Blueprint("file", __name__, url_prefix="/api/v1/files")
 
-@file_bp.route("/", methods=["GET"])
+@file_attachment_bp.route("/", methods=["GET"])
 def list_all_notifications():
     repo = RepositoryFactory.get_repository("file")
     files = repo.get_all()
