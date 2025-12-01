eaf23971baee49eb8cb6d496d18a39f13c65ab9a
Ahmed Mohsen <ahmedazab05@gmail.com>
2025-12-01 14:47:02 +0200

    small edit

--- src/models/AI_engine.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-class Ai_Engine:
-    def __init__(self, engine_id, modelVersion, channel, lastTrained):
-        self.engine_id: int = engine_id
-        self.modelVersion: str = modelVersion
-        self.lastTrained: datetime.date = lastTrained
