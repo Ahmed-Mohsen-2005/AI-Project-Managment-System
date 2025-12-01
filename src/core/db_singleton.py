1d2190f66bf5cb48847b060b82381774ac0c05b3
Ahmed Mohsen <ahmedazab05@gmail.com>
2025-12-01 15:39:18 +0200

    define db_singleton

---
+++ src/core/db_singleton.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseConnection:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+        return cls._instance
+    def __init__(self):
+        if not hasattr(self, "connection") or self.connection is None:
+            try:
+                self.connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="", 
+                database="AIPMS"
+                )
+                print(" Connected to MySQL")
+            except Error as e:
+                print(" ERROR connecting to MySQL:", e)
+                self.connection = None
+
+def get_connection(self):
+    return self.connection
